7c148e0478ae673a0577902bf0df4c0c67c355e4
Yoiro <degrevesim@gmail.com>
2020-01-22 20:01:25 +0100

    Create todoItemComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TodoListComponent ],
+  declarations: [ AppComponent, HelloComponent, TodoListComponent, TodoItemComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
